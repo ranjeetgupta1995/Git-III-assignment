2e2fc84409eccaa0b5a9b315d71fb9b5c9cd2710
manoranjan jena <jmanoranjan180@gmail.com>
2023-01-11 14:31:42 +0530

    code is fixed

--- primeproblem.js
+++ primeproblem.js
@@ -1,12 +1,17 @@
 // to get prime
 
+// to get prime
+
 let num=31;
-if(num<2){
-return true;
+let count = 0;
+for(let i = 1;i<=num;i++){
+    if(num%i==0){
+        count++;
+    }
 }
-for(let i=2; i<=Math.sqrt(num); i++){
-if(num%i==0){
-return false;
+
+if(count >= 2){
+    console.log('this is a prime');
+}else{
+    console.log('this is not a prime');
 }
-}
-return true;
